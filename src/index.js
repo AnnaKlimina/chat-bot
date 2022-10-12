7559f1e0d36696799cfe2e5d887a470b54578ac2
AnnaKlimina <AnnaKlimina@users.noreply.github.com>
2022-10-12 13:27:15 +0300

    chat-bot-commit

--- src/index.js
+++ src/index.js
@@ -1,72 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import "./styles_v1/index.css";
-// import logo from "./img_v1/logo.png";
-// import botIcon from "./img_v1/bot.png";
+import "./styles/index.css";
+import logo from "./img/logo.png";
+import botIcon from "./img/bot.png";
 
-import "./styles_v2/index.css";
-import logo from "./img_v2/logo.png";
-import botIcon from "./img_v2/bot.png";
+const serverLink = "http://localhost:8080";
 
-class Message extends React.Component {
-  render() {
-    let element = null;
-    const options = this.props.options;
-
-    if (options.appear && this.props.position === "_bot") {
-      if (options.category === "_text-option") {
-        element = (
-          <div className="option-container">
-            {options.array.map((text, index) => (
-              <Option
-                key={index}
-                option={options.category}
-                text={text[0]}
-                next={[text[1]]}
-                onClick={(e) => {
-                  this.props.onClick(e, text[1]);
-                }}
-              />
-            ))}
-          </div>
-        );
-      } else {
-        element = (
-          <div className="option-container">
-            <Option
-              option="_yes-option"
-              text="Да"
-              onClick={(e) => {
-                this.props.onClick(e);
-              }}
-            />
-            <Option
-              option="_no-option"
-              text="Нет"
-              onClick={(e) => {
-                this.props.onClick(e);
-              }}
-            />
-          </div>
-        );
-      }
-    }
-
-    return (
-      <div className={"message-container " + this.props.position}>
-        <div className="message">{this.props.text}</div>
-        {element}
-      </div>
-    );
+class MessageObject {
+  constructor(
+    text,
+    sender,
+    options = { appear: false, array: [] },
+    link = null
+  ) {
+    this.text = text;
+    this.sender = "_" + sender;
+    this.options = options;
+    this.link = link;
   }
 }
+
+const startBlock = new MessageObject(
+  "Добро пожаловать! Давайте я покажу вам, с чем могу помочь:",
+  "bot",
+  {
+    appear: true,
+    array: [
+      ["Пройти тест на самозанятость", "/selfTest/yes"],
+      ["Получить комплект докуметов", "/documents/doc"],
+      ["Узнать подробнее про самозанятых", 5],
+    ],
+  }
+);
+const continueBlock = JSON.parse(JSON.stringify(startBlock));
+continueBlock.text = "Давайте я покажу вам, с чем могу помочь:";
 
 class Option extends React.Component {
   render() {
     return (
       <div
-        className={"option " + this.props.option}
+        className="option"
         onClick={(e) => {
           this.props.onClick(e);
         }}
@@ -77,34 +51,58 @@ class Option extends React.Component {
   }
 }
 
+class Message extends React.Component {
+  render() {
+    const options = this.props.options;
+
+    let element = null;
+    if (options.appear) {
+      element = (
+        <div className="option-container">
+          {options.array.map((text, index, array) => (
+            <Option
+              key={index}
+              text={text[0]}
+              onClick={(e) => {
+                this.props.onClick(
+                  e,
+                  text[1],
+                  index === array.length - 1 ? -1 : index
+                );
+              }}
+            />
+          ))}
+        </div>
+      );
+    }
+
+    let link = null;
+    if (this.props.link) {
+      link = (
+        <a href={this.props.link} className="message__link">
+          Скачать
+        </a>
+      );
+    }
+
+    return (
+      <div className={"message-container " + this.props.position}>
+        <div className="message">
+          {this.props.text}
+          <br />
+          {link}
+        </div>
+        {element}
+      </div>
+    );
+  }
+}
+
 class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: [
-        {
-          text: "Добро пожаловать! Давайте я покажу вам, с чем могу помочь:",
-          sender: "_bot",
-          options: {
-            appear: true,
-            category: "_text-option",
-            array: [
-              ["Пройти тест на самозанятость", 5],
-              ["Получить комплект докуметов", 1],
-              ["Узнать подробнее про самозанятых", 5],
-            ],
-          },
-        },
-        // {
-        //   text: "Вопрос теста:...?",
-        //   sender: "_bot",
-        //   options: {
-        //     appear: true,
-        //     category: "_test-option",
-        //     array: [],
-        //   },
-        // },
-      ],
+      messages: [JSON.parse(JSON.stringify(startBlock))],
     };
   }
 
@@ -119,8 +117,9 @@ class Chat extends React.Component {
             text={m.text}
             position={m.sender}
             options={m.options}
-            onClick={(e, i) => {
-              this.handleClick(e, i);
+            link={m.link}
+            onClick={(e, link, index) => {
+              this.handleClick(e, link, index);
             }}
           />
         ))}
@@ -128,27 +127,88 @@ class Chat extends React.Component {
     );
   }
 
-  handleClick(e, index) {
+  async fetchSETest(messages, link) {
+    let newQuestion = "Извините, сервер временно недоступен";
+    let response = await fetch(serverLink + link);
+
+    if (response.ok) {
+      newQuestion = await response.json();
+      messages.push(
+        new MessageObject(newQuestion.text, "bot", {
+          appear: newQuestion.question,
+          array: newQuestion.question
+            ? [
+                ["Да", "/selfTest/yes"],
+                ["Нет", "/selfTest/no"],
+                ["Назад", "/selfTest/reject"],
+              ]
+            : [],
+        })
+      );
+
+      if (!newQuestion.question) {
+        messages.push(JSON.parse(JSON.stringify(continueBlock)));
+      }
+    } else {
+      messages.push(new MessageObject(newQuestion, "bot"));
+      messages.push(JSON.parse(JSON.stringify(continueBlock)));
+    }
+  }
+
+  async fetchDocTest(messages, link, index) {
+    if (index === -1) {
+      messages.push(JSON.parse(JSON.stringify(continueBlock)));
+      return;
+    }
+    let newQuestion = "Извините, сервер временно недоступен";
+    let response = await fetch(
+      serverLink + link + "?" + new URLSearchParams({ bodyNumber: index })
+    );
+    if (response.ok) {
+      newQuestion = await response.json();
+      if (newQuestion.text !== "/download") {
+        let options = newQuestion.answers.map((text) => [
+          text,
+          "/documents/doc",
+        ]);
+        messages.push(
+          new MessageObject(newQuestion.text, "bot", {
+            appear: true,
+            array: options,
+          })
+        );
+      } else {
+        messages.push(
+          new MessageObject(
+            "Ваш договор:",
+            "bot",
+            { appear: false, array: [] },
+            "http://localhost:8080/download"
+          )
+        );
+        messages.push(JSON.parse(JSON.stringify(continueBlock)));
+      }
+    } else {
+      messages.push(new MessageObject(newQuestion, "bot"));
+      messages.push(JSON.parse(JSON.stringify(continueBlock)));
+    }
+  }
+
+  async handleClick(e, link, index) {
     const messages = this.state.messages;
 
     let messageOptions = messages[messages.length - 1].options;
 
     messageOptions.appear = false;
-    messageOptions.category = "";
     messageOptions.array = [];
 
-    messages.push({
-      text: e.target.textContent,
-      sender: "_user",
-      options: {
-        appear: false,
-        category: "",
-        array: [],
-      },
-    });
+    messages.push(new MessageObject(e.target.textContent, "user"));
 
-    let obj = JSON.parse(JSON.stringify(test[index]));
-    messages.push(obj);
+    if (link.includes("selfTest")) {
+      await this.fetchSETest(messages, link);
+    } else {
+      await this.fetchDocTest(messages, link, index);
+    }
 
     this.setState({ messages: messages });
 
@@ -156,21 +216,6 @@ class Chat extends React.Component {
       let element = document.querySelector(".chat-wrapper");
       element.scrollTop = element.scrollHeight;
     }, 0);
-  }
-}
-
-class Page extends React.Component {
-  render() {
-    return (
-      <div className="page">
-        <div className="logo">
-          <img className="logo__img" src={logo} alt="logo" />
-        </div>
-        <ChatBot />
-        <div className="block"></div>
-        <div className="footer-img"></div>
-      </div>
-    );
   }
 }
 
@@ -185,79 +230,18 @@ class ChatBot extends React.Component {
   }
 }
 
+class Page extends React.Component {
+  render() {
+    return (
+      <div className="page">
+        <div className="logo">
+          <img className="logo__img" src={logo} alt="logo" />
+        </div>
+        <ChatBot />
+      </div>
+    );
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Page />);
-
-let test = [
-  {
-    text: "Добро пожаловать! Давайте я покажу вам, с чем могу помочь:",
-    sender: "_bot",
-    options: {
-      appear: true,
-      category: "_text-option",
-      array: [
-        ["Пройти тест на самозанятость", 5],
-        ["Получить комплект докуметов", 1],
-        ["Узнать подробнее про самозанятых", 5],
-      ],
-    },
-  },
-  {
-    text: "Что вы хотите делать?",
-    sender: "_bot",
-    options: {
-      appear: true,
-      category: "_text-option",
-      array: [
-        ["Сдавать свой автомобиль в аренду", 2],
-        ["Ремонтировать автомобиль", 5],
-        ["Работать таксистом", 5],
-        ["Назад", 0],
-      ],
-    },
-  },
-  {
-    text: "Вы собираетесь возить арендатора на вашем автомобиле?",
-    sender: "_bot",
-    options: {
-      appear: true,
-      category: "_text-option",
-      array: [
-        ["Нет", 3],
-        ["Да", 5],
-        ["Назад", 1],
-      ],
-    },
-  },
-  {
-    text: "Кто вы?",
-    sender: "_bot",
-    options: {
-      appear: true,
-      category: "_text-option",
-      array: [
-        ["Самозанятый без статуса ИП", 4],
-        ["Самозанятый со статусом ИП", 4],
-        ["Назад", 2],
-      ],
-    },
-  },
-  {
-    text: "Тут будет ссылка на договор",
-    sender: "_bot",
-    options: {
-      appear: true,
-      category: "_text-option",
-      array: [["Вернуться на стартовый блок", 0]],
-    },
-  },
-  {
-    text: "Тут будет переход к другому тесту",
-    sender: "_bot",
-    options: {
-      appear: true,
-      category: "_text-option",
-      array: [["Вернуться на стартовый блок", 0]],
-    },
-  },
-];
